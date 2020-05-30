8343bbf953d48fa2a0cc2ee75fb8f47d981f7bd0
Jyoti-uio <jyoti.uiodevelopment@gmail.com>
2020-05-30 22:23:13 +0530

    file uploaded

---
+++ common-pages/footer.php
@@ -0,0 +1,128 @@
+<footer class="footer">
+
+<!-- Footer Top -->
+    <div class="footer-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-about">
+                        <div class="footer-logo">
+                            <a href="index.php"><img src="assets\img\footer-logo.png" alt="logo"></a>
+                        </div>
+                        <div class="footer-about-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <div class="social-icon">
+                                <ul>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-facebook-f"></i> </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-twitter"></i> </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-menu">
+                        <h2 class="footer-title">Services</h2>
+                        <ul>
+                            <li><a href="#">Homeopathic</a></li>
+                            <li><a href="#">Skin & Hair</a></li>
+                            <li><a href="#">Nutrition</a></li>
+                            <li><a href="#">Weight Gain & Weight Loss</a></li>
+                        </ul>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-menu">
+                        <h2 class="footer-title">For Patients</h2>
+                        <ul>
+                            <li><a href="#">Appointments</a></li>
+                            <li><a href="register.php">Register</a></li>
+                            <li><a href="prescription.php">Prescription</a></li>
+                        </ul>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-contact">
+                        <h2 class="footer-title">Contact Us</h2>
+                        <div class="footer-contact-info">
+                            <div class="footer-address">
+                                <span><i class="fas fa-map-marker-alt"></i></span>
+                                <p> 3556 Beech Street, San Francisco,<br> California, CA 94108 </p>
+                            </div>
+                            <p>
+                                <i class="fas fa-phone-alt"></i><a href="#">+1 315 369 5943</a>
+                            </p>
+                            <p class="mb-0">
+                                <i class="fas fa-envelope"></i><a href="#">doccure@example.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+<!-- /Footer Top -->
+
+<!-- Footer Bottom -->
+<div class="footer-bottom">
+    <div class="container-fluid">
+
+        <!-- Copyright -->
+        <div class="copyright">
+            <div class="row">
+                <div class="col-md-6 col-lg-6">
+                    <div class="copyright-text">
+                        <a href="#">&copy; 2020 Doccure.</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6">
+
+                    <!-- Copyright Menu -->
+                    <div class="copyright-menu">
+                        <ul class="policy-menu">
+                            <li><a href="term-condition.php">Terms and Conditions</a></li>
+                            <li><a href="privacy-policy.php">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <!-- /Copyright Menu -->
+
+                </div>
+            </div>
+        </div>
+        <!-- /Copyright -->
+
+    </div>
+</div>
+<!-- /Footer Bottom -->
+
+</footer>
---
+++ common-pages/header.php
@@ -0,0 +1,79 @@
+
+        <!-- Header -->
+        <header class="header">
+            <nav class="navbar navbar-expand-lg header-nav">
+                <div class="navbar-header">
+                    <a id="mobile_btn" href="javascript:void(0);">
+                        <span class="bar-icon">
+								<span></span>
+                        <span></span>
+                        <span></span>
+                        </span>
+                    </a>
+                    <a href="index.php" class="navbar-brand logo">
+                        <img src="assets\img\logo.png" class="img-fluid" alt="Logo">
+                    </a>
+                </div>
+                <div class="main-menu-wrapper">
+                    <div class="menu-header">
+                        <a href="index.html" class="menu-logo">
+                            <img src="assets\img\logo.png" class="img-fluid" alt="Logo">
+                        </a>
+                        <a id="menu_close" class="menu-close" href="javascript:void(0);">
+                            <i class="fas fa-times"></i>
+                        </a>
+                    </div>
+                    <ul class="main-nav">
+                        <li class="active">
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li class="has-submenu">
+                            <a href="">Services <i class="fas fa-chevron-down"></i></a>
+                            <ul class="submenu">
+                                <li><a href="#">Homeopathic</a></li>
+                                <li><a href="#">Dermatology</a></li>
+                                <li><a href="#">Nutrition</a></li>
+                                <li><a href="#">Weight gain & Loss</a></li>
+                            </ul>
+                        </li>
+                        <li class="has-submenu">
+                            <a href="#">Patients</a>
+                            <!-- <ul class="submenu">
+                                <li class="has-submenu">
+                                    <a href="#">Doctors</a>
+                                    <ul class="submenu">
+                                        <li><a href="map-grid.html">Map Grid</a></li>
+                                        <li><a href="map-list.html">Map List</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="search.html">Search Doctor</a></li>
+                                <li><a href="doctor-profile.html">Doctor Profile</a></li>
+                                <li><a href="booking.html">Booking</a></li>
+                                <li><a href="checkout.html">Checkout</a></li>
+                                <li><a href="booking-success.html">Booking Success</a></li>
+                                <li><a href="patient-dashboard.html">Patient Dashboard</a></li>
+                                <li><a href="favourites.html">Favourites</a></li>
+                                <li><a href="chat.html">Chat</a></li>
+                                <li><a href="profile-settings.html">Profile Settings</a></li>
+                                <li><a href="change-password.html">Change Password</a></li>
+                            </ul> -->
+                        </li>
+                        <li class="has-submenu">
+                            <a href="#">Pages</a>
+                        </li>
+                    </ul>
+                </div>
+                <ul class="nav header-navbar-rht">
+                    <li class="nav-item contact-item">
+                        <div class="header-contact-img">
+                            <i class="far fa-hospital"></i>
+                        </div>
+                        <div class="header-contact-detail">
+                            <p class="contact-header">Contact</p>
+                            <p class="contact-info-header"> +1 315 369 5943</p>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <!-- /Header -->
